91b7e472e0f7de843abfc1dbbb6b2952453ba19c
BenQUUU <michal.kolczak123@gmail.com>
2024-03-04 19:13:46 +0100

    Create a parent class Person

---
+++ include/person.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+#include "../include/json.hpp"
+
+using json = nlohmann::json;
+
+enum class Gender {
+    Male,
+    Female
+};
+
+inline Gender getGenderFromInput(const std::string& input) {
+    if (input == "Male") {
+        return Gender::Male;
+    } else if (input == "Female") {
+        return Gender::Female;
+    } else {
+        return Gender::Male;
+    }
+}
+
+class Person {
+public:
+    Person();
+    Person(const std::string& firstName,
+           const std::string& lastName,
+           const std::string& address,
+           const std::string& pesel,
+           Gender gender);
+    virtual ~Person();
+
+    std::string getLastName() const;
+    std::string getPESEL() const;
+    std::string getGenderAsString() const;
+
+    virtual std::string show() const = 0;
+    virtual int getIndexNumber() const = 0;
+    virtual double getSalary() const = 0;
+    virtual bool setSalary(double salary) = 0;
+
+    bool verifyPESEL(const std::string& pesel);
+
+    virtual json to_json() const = 0;
+    virtual void from_json(const json& j) = 0;
+
+protected:
+    std::string firstName_;
+    std::string lastName_;
+    std::string address_;
+    std::string pesel_;
+    Gender gender_;
+};
---
+++ src/person.cpp
@@ -0,0 +1,56 @@
+#include "../include/person.hpp"
+
+Person::Person() {
+}
+
+Person::Person(const std::string& firstName,
+               const std::string& lastName,
+               const std::string& address,
+               const std::string& pesel,
+               Gender gender)
+    : firstName_(firstName),
+      lastName_(lastName),
+      address_(address),
+      pesel_(pesel),
+      gender_(gender) {
+}
+
+Person::~Person() {
+}
+
+std::string Person::getLastName() const {
+    return lastName_;
+}
+
+std::string Person::getPESEL() const {
+    return pesel_;
+}
+
+std::string Person::getGenderAsString() const {
+    switch (gender_) {
+    case Gender::Male:
+        return "Male";
+    case Gender::Female:
+        return "Female";
+    }
+
+    return "Unknown";
+}
+
+bool Person::verifyPESEL(const std::string& pesel) {
+    const std::vector digitWeight{1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+    int result = 0;
+    int checkDigit = 0;
+
+    if (pesel.length() != 11) {
+        return false;
+    }
+
+    for (size_t i = 0; i < digitWeight.size(); i++) {
+        result += digitWeight.at(i) * (pesel[i] - '0');
+    }
+
+    checkDigit = (10 - (result % 10)) % 10;
+
+    return checkDigit == pesel[10] - '0';
+}
